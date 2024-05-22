bda0f358d2495397931be3a3a6a6b99019121b7c
anvarbekov <mustafoanvarbekov24@gmail.com>
2024-05-22 19:43:48 +0500

    api category updated

--- app/api/products/categories/route.ts
+++ app/api/products/categories/route.ts
@@ -4,5 +4,7 @@ import ProductModel from '@/lib/models/ProductModel'
 export const GET = async (req: any) => {
   await dbConnect()
   const categories = await ProductModel.find().distinct('category')
-  return Response.json(categories)
+  return new Response(JSON.stringify(categories), {
+    headers: { 'Content-Type': 'application/json' }
+  })
 }
