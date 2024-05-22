e81835b5fa7fab045b1f2c74796dcdda4eb30e37
anvarbekov <mustafoanvarbekov24@gmail.com>
2024-05-22 19:59:09 +0500

    next add

--- app/api/products/categories/route.ts
+++ app/api/products/categories/route.ts
@@ -1,10 +1,9 @@
 import dbConnect from '@/lib/dbConnect'
 import ProductModel from '@/lib/models/ProductModel'
+import { NextResponse } from 'next/server' // Next.js framework uchun kerakli import
 
 export const GET = async (req: any) => {
   await dbConnect()
   const categories = await ProductModel.find().distinct('category')
-  return new Response(JSON.stringify(categories), {
-    headers: { 'Content-Type': 'application/json' }
-  })
+  return NextResponse.json(categories) // Next.js javobini qaytarish
 }
