2be1a35abd3c138d28916651e9fb65d8d8993c17
anvarbekov <mustafoanvarbekov24@gmail.com>
2024-05-22 19:45:18 +0500

    data

--- lib/data.ts
+++ lib/data.ts
@@ -1,29 +1,29 @@
-// import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs'
 
-// const data = {
-//   users: [
-//     {
-//       name: 'Admin',
-//       email: 'admin@example.com',
-//       password: bcrypt.hashSync('admin01'),
-//       isAdmin: true,
-//     },
-//     {
-//       name: 'John',
-//       email: 'user@example.com',
-//       password: bcrypt.hashSync('12345'),
-//       isAdmin: false,
-//     },
-//   ],
-//   products: [
-//     {
-//       name: 'Yangi yil',
-//       slug: 'news-0',
-//       category: 'New Year',
-//       image: 'https://qum2khm.uz/wp-content/uploads/2023/12/8-1.jpg',
-//       description: 'Eng quvnoq bayram',
-//     }
-// ]
-// }
+const data = {
+  users: [
+    {
+      name: 'Admin',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('admin01'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('12345'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      name: 'Yangi yil',
+      slug: 'news-0',
+      category: 'New Year',
+      image: 'https://qum2khm.uz/wp-content/uploads/2023/12/8-1.jpg',
+      description: 'Eng quvnoq bayram',
+    }
+]
+}
 
-// export default data
+export default data
