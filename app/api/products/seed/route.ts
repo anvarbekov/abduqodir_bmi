54acd560e286057593eb85e3fdbd6705781cbbc9
anvarbekov <mustafoanvarbekov24@gmail.com>
2024-05-22 15:36:36 +0500

    update api/product/seed/route.ts

--- app/api/products/seed/route.ts
+++ app/api/products/seed/route.ts
@@ -5,17 +5,28 @@ import UserModel from '@/lib/models/UserModel'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (request: NextRequest) => {
-  const { users, products } = data
-  await dbConnect()
-  await UserModel.deleteMany()
-  await UserModel.insertMany(users)
-
-  await ProductModel.deleteMany()
-  await ProductModel.insertMany(products)
-
-  return NextResponse.json({
-    message: 'seeded successfully',
-    users,
-    products,
-  })
+  try {
+    const { users, products } = data
+    
+    // Bazaga ulangan bo'lsangiz, bog'lanishni aniqlang
+    await dbConnect()
+    
+    // Oldingi ma'lumotlarni o'chirib tashlang
+    await UserModel.deleteMany()
+    await ProductModel.deleteMany()
+    
+    // Yangi ma'lumotlarni bazaga joylang
+    await UserModel.insertMany(users)
+    await ProductModel.insertMany(products)
+    
+    // Agar hamma narsa to'g'ri bo'lsa, muvaffaqiyatli qaytarish
+    return NextResponse.json({
+      message: 'Seed muvaffaqiyatli amalga oshirildi',
+      users,
+      products,
+    })
+  } catch (error) {
+    // Agar xato bo'lsa uni qaytarish
+    return NextResponse.json({ error: error })
+  }
 }
