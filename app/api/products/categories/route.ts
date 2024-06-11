1ef7d02932fb32513df476276ee982c2457093cd
anvarbekov <mustafoanvarbekov24@gmail.com>
2024-06-11 10:08:56 +0500

    chat gpt dan yangilandi

--- app/api/products/categories/route.ts
+++ app/api/products/categories/route.ts
@@ -1,8 +1,9 @@
 import dbConnect from '@/lib/dbConnect'
 import ProductModel from '@/lib/models/ProductModel'
+import { NextResponse } from 'next/server'
 
 export const GET = async (req: any) => {
   await dbConnect()
   const categories = await ProductModel.find().distinct('category')
-  return Response.json(categories)
+  return NextResponse.json(categories)
 }
